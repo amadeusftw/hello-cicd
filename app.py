7cd9139061c6e56a941bbfea7524b1c8770e96ef
Emilie de Paula <87emidep@mk.se>
2026-02-03 15:01:44 +0100

    Fix: bind Flask to 0.0.0.0 and use env vars

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', 'false').lower() in ('1', 'true', 'yes')
+    app.run(host=host, port=port, debug=debug)
